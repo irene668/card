49131452fd655dce2446529d8c813c696dc80a69
Jing <yui_lai@bankpro.com.tw>
2024-08-13 17:39:57 +0800

    new project

---
+++ src/test/java/com/jing/card/CardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jing.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
